d221cf2f263810ed929e1a37aa9718e2f9e58b73
xxf185 <109097973+xxf185@users.noreply.github.com>
2024-06-19 15:29:47 +0800

    Update core.sh

--- src/core.sh
+++ src/core.sh
@@ -784,8 +784,7 @@ uninstall() {
     fi
     [[ $is_install_sh ]] && return # reinstall
     _green "\n卸载完成!"
-    msg "脚本哪里需要完善? 请反馈"
-    msg "反馈问题) $(msg_ul https://github.com/${is_sh_repo}/issues)\n"
+    msg ""
 }
 
 # manage run status
